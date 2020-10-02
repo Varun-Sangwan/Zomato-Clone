c060c1623a298afbc637ac12e23648b884cd6841
varun-sa <71647108+varun-sa@users.noreply.github.com>
2020-10-02 13:58:22 +0530

    added otp verification

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const loc = document.getElementById("location");
 
 const loginUser = document.querySelector(".login");
+
 const closeModal = document.querySelector(".close-modal");
 const signUpUser = document.querySelector(".signup");
 const closeSignup = document.querySelector(".close-signup");
+const closeOtp = document.querySelector(".close-otp");
 
 const loginLink = document.querySelector(".login-link");
 const signupLink = document.querySelector(".signup-link");
@@ -25,6 +27,9 @@ document.getElementById("selected-location").innerText = loc.value;
 const backToLogin = document.querySelector(".back-to-login");
 const closeEmailModal = document.querySelector(".close-email-modal");
 const sendPasswordEmail = document.querySelector(".send-password-to-email");
+
+const resendOtp = document.querySelector(".send-otp");
+let count = 30;
 
 function locationsDisplay() {
   event.preventDefault();
@@ -77,11 +82,17 @@ function closeModalGroup() {
   document.querySelector(".signup-modal-class").style.display = "none";
   document.querySelector(".login-modal-class").style.display = "none";
   document.querySelector(".send-pswrd-to-email").style.display = "none";
+  document.querySelector(".otpverification").style.display = "none";
 }
 
 closeModal.addEventListener("click", closeModalGroup);
 closeSignup.addEventListener("click", closeModalGroup);
 closeEmailModal.addEventListener("click", closeModalGroup);
+closeOtp.addEventListener("click", closeModalGroup);
+document.querySelector(".logout").addEventListener("click", () => {
+  console.log("reload reqnf");
+  location.reload();
+});
 
 createAccnt.addEventListener("click", (event) => {
   event.preventDefault();
@@ -119,7 +130,10 @@ sendPswrd.addEventListener("click", (event) => {
   if (mobile) {
     let registeredUser = users.filter((data) => data.mobile === mobile);
     if (registeredUser.length > 0) {
-      loginAlert.innerHTML = `Password sent succesfully to ${registeredUser[0].name}`;
+      document.querySelector("#username").innerText = registeredUser[0].name;
+      document.querySelector(".login-modal-class").style.display = "none";
+      document.querySelector(".otpverification").style.display = "block";
+      otpTimer(count);
     } else {
       loginAlert.innerHTML = `This user is not registered with us ,Kindly proceed to signup`;
     }
@@ -145,7 +159,11 @@ sendPasswordEmail.addEventListener("click", () => {
   if (email) {
     let registeredUser = users.filter((data) => data.email === email);
     if (registeredUser.length > 0) {
-      emailAlert.innerHTML = `Password sent succesfully to ${registeredUser[0].email}`;
+      console.log(registeredUser);
+      document.querySelector("#username").innerText = registeredUser[0].name;
+      document.querySelector(".send-pswrd-to-email").style.display = "none";
+      document.querySelector(".otpverification").style.display = "block";
+      otpTimer(30);
     } else {
       emailAlert.innerHTML = `This user is not registered with us ,Kindly proceed to signup`;
     }
@@ -155,6 +173,39 @@ sendPasswordEmail.addEventListener("click", () => {
 
   document.querySelector("#email-id").value = "";
 });
+
+resendOtp.addEventListener("click", () => {
+  if (document.querySelector("#timer").innerText === "00") {
+    otpTimer(30);
+  }
+});
+
+function otpTimer(count) {
+  let counter = setInterval(() => {
+    count--;
+    if (count <= 0) {
+      document.querySelector("#timer").innerText = "00";
+      clearInterval(counter);
+      return;
+    }
+    document.querySelector("#timer").innerText = count;
+  }, 1000);
+
+  const otpInputs = document.querySelectorAll("#otp");
+  const password = "123456";
+  const userPassword = [];
+  otpInputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      userPassword.push(input.value);
+      let userPass = userPassword.join("");
+      if (userPass === password) {
+        document.querySelector(".user-logged-in").style.display = "block";
+        document.querySelector(".login-div").style.display = "none";
+        document.querySelector(".otpverification").style.display = "none";
+      }
+    });
+  });
+}
 
 document.querySelector(".login-modal").addEventListener("click", () => {
   event.stopPropagation();
@@ -167,6 +218,11 @@ document.querySelector(".signup-modal").addEventListener("click", () => {
 document.querySelector(".email-modal").addEventListener("click", () => {
   event.stopPropagation();
 });
+
+document.querySelector(".otpverification").addEventListener("click", () => {
+  event.stopPropagation();
+});
+
 document.addEventListener("click", (event) => {
   event.preventDefault();
   dropDownDiv.style.display = "none";
